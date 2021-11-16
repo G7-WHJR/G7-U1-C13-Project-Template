efeb39111cb9e8f7f07e8152173dbea3e90d29ef
unknown <raveen.anand@whitehatjr.com>
2021-11-16 19:30:55 +0530

    v2

--- Best Friends Array/sketch.js
+++ Best Friends Array/sketch.js
@@ -1,16 +1,11 @@
-//Declare and initialize an array named friends
+//1. Declare and initialize an array named friends
 
 function setup() {
-  //size of the canvas to draw on
   createCanvas(innerWidth, innerHeight);
-
-  // Set the background to white, and move the line to setup();
   background("white");
-  
   console.log("My best friends are :");
 
- 
-  //print the names in friends array to the console using console.log and for loop
+  //2. print the names in friends array in the console:
   
 
 }
